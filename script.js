86c0355cecc7c6c5563da5fdb65c5f0abf7162e1
Maks13km <99037653+Maks13km@users.noreply.github.com>
2022-03-09 16:13:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 let notes = []
+let toDo = {}
+let priority = "High"
+let toDisplay = ""
 let description = ""
 
-function printNotes() {
-    let toDisplay = ""
-    for (let i in notes) {
-
-        toDisplay += notes[i].name
-
+function validate(description) { //функция валидации
+    console.log(description.trim()) //метод trim удаляет лишние пробелы
+    let error = document.querySelector(".error_container")
+    if (description == "") {
+        error.innerHTML = "Введите заметку"
+        return false
     }
+    // for (let i of notes) {
+    //     if (value == i.description) {
+    //         error.innerHTML = "Такая заметка уже существует"
+    //         return false //проверка на то ,существует ли уже заметка с таким именем
+    //     }
+    // }
+    error.innerHTML = "" //никаких ошибок нет, все ОК
+    return true
 }
 
-function saveToLocale(key, obj) {
+function saveToLocale(key, obj) {  // сохранение в локал сторидж
     if (obj) {
         localStorage.setItem(key, JSON.stringify(obj))
     } else {
@@ -18,25 +29,56 @@ function saveToLocale(key, obj) {
     }
 }
 
-function getFromLocale(key) {
+function getFromLocale(key) {  // получение из локал сториджа
     return JSON.parse(localStorage.getItem(key))
 }
 
+function deleteNote(index) {
+    notes.splice(index, 1)
+    saveToLocale("notes", notes)
+    printNotes()
+}
+
+function printNotes() {
+    let toDisplay = ""
+    for (let i in notes) {
+        toDisplay += '<br>' + notes[i].description
+    }
+    document.querySelector('.notes_container').innerHTML = toDisplay
+        // let deleteButtons = document.getElementsByClassName("delete")
+        // for (let i in [...deleteButtons]) {
+        //     deleteButtons[i].addEventListener("click", () => {
+        //         deleteNote(i)
+        //     })
+        // }
+}
 
 document.addEventListener("DOMContentLoaded", function() {
     if (getFromLocale("notes")) {
         notes = getFromLocale("notes")
     }
     printNotes()
-})
 
-document.querySelector(".btn").addEventListener("click", () => {
-    let toDoElem = document.querySelectorAll(".toDo")
-    toDo = []
-    for (let i of toDoElem) {
-        toDo.push({
-            value: i.value,
-            done: false
-        })
-    }
+
+    document.querySelector(".addCircle").addEventListener("click", () => {
+        description = document.querySelector("#description").value
+        toDo.description = description.trim()
+        toDo.priority = priority
+        toDo.date = Date.now()
+
+        if (validate(toDo.description)) {
+            notes.push(toDo)
+            toDo = {}
+        }
+        console.log(notes)
+        saveToLocale("notes", notes)
+        printNotes()
+        document.querySelector("#description").value = ""
+
+    })
+
+    document.querySelector(".priority").addEventListener("change", (e) => {
+        priority = e.target.value
+
+    })
 })
